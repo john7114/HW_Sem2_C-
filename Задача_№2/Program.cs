81320e0e794f885cd189edd03c4dd7f9f09c7119
Tulkinzhon Baymatov <john7.42.108@gmail.com>
2022-12-05 20:12:10 +0300

    Программа, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.

---
+++ Задача_№2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число: ");
+string a = Console.ReadLine();
+
+int no_third_digit = Convert.ToInt32(a);
+
+if (no_third_digit < 99) 
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+int third_digit = (int)Char.GetNumericValue(a[2]);
+
+Console.WriteLine(third_digit);
