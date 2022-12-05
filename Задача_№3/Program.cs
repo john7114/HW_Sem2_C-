b83ecef03618a1fc83f4e500e83a7316a48fd32d
Tulkinzhon Baymatov <john7.42.108@gmail.com>
2022-12-05 20:45:47 +0300

    Программа, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.

---
+++ Задача_№3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 7 == 0) 
+if (number % 23 == 0) 
+{
+    Console.WriteLine("Введённое число кратно одновременно 7 и 23");
+}
+else
+{
+    Console.WriteLine("Введённое число не кратно одновременно 7 и 23");
+}
